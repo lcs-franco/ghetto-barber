6fae2769a701c86905ca4bcc5e76a98677338dea
Lucas <85856491+Xugnon@users.noreply.github.com>
2024-01-30 15:27:18 -0300

    fix: alter bookingItem file name

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,7 +2,7 @@ import { format } from 'date-fns';
 import { Header } from '../_components/header';
 import { ptBR } from 'date-fns/locale';
 import { Search } from './_components/search';
-import { BookingItem } from '../_components/booking-item';
+import { BookingItem } from '../_components/bookingItem';
 
 export default function Home() {
   return (
