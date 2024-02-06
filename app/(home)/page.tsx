bb26bee361f63c40cbd6fb93e531531b2d6ffdd4
Lucas <85856491+Xugnon@users.noreply.github.com>
2024-02-06 18:25:12 -0300

    feat: ensure confirmed bookings are shown on home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,10 +5,29 @@ import Header from '../_components/header';
 import Search from './_components/search';
 import BookingItem from '../_components/bookingItem';
 import BarbershopItem from './_components/barbershopItem';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../api/auth/[...nextauth]/route';
 
 export default async function Home() {
-  //call prisma and get barbershops
-  const barbershops = await db.barbershop.findMany({});
+  const session = await getServerSession(authOptions);
+
+  const [barbershops, confirmedBookings] = await Promise.all([
+    db.barbershop.findMany({}),
+    session?.user
+      ? db.booking.findMany({
+          where: {
+            userId: (session.user as any).id,
+            date: {
+              gte: new Date(),
+            },
+          },
+          include: {
+            service: true,
+            barbershop: true,
+          },
+        })
+      : Promise.resolve([]),
+  ]);
 
   return (
     <div>
@@ -27,12 +46,16 @@ export default async function Home() {
         <Search />
       </div>
 
-      {/* <div className="px-5 mt-6">
-        <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">
+      <div className="mt-6">
+        <h2 className="pl-5 text-xs mb-3 uppercase text-gray-400 font-bold">
           Agendamentos
         </h2>
-        <BookingItem />
-      </div> */}
+        <div className="px-5 flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {confirmedBookings.map((booking) => (
+            <BookingItem key={booking.id} booking={booking} />
+          ))}
+        </div>
+      </div>
 
       <div className="mt-6">
         <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">
